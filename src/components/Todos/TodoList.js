6ecfb5277f2e046eb4a090f32c90e90ada793b60
Ivan Ivanov <ivan.ivanov@example.com>
2025-03-01 12:37:14 +0200

    Adding styles to the input form and task form

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,7 +1,15 @@
+import styles from './TodoList.module.css';
 import Todo from './Todo';
 
 function TodoList({ todos }) {
-  return todos.map((todo, index) => <Todo key={index} todo={todo} />);
+  return (
+    <div className={styles.todoListConteiner}>
+      {!todos.length && <h2>Todo list is empty</h2>}
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todo} />
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;
