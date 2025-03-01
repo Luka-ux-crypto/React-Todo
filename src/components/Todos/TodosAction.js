e6d28ce47c0e9714aba5050dc491dafc26c5dd09
Ivan Ivanov <ivan.ivanov@example.com>
2025-03-01 18:28:59 +0200

    Hiding the delete completed tasks button. Reusing a Component Button

--- src/components/Todos/TodosAction.js
+++ src/components/Todos/TodosAction.js
@@ -1,16 +1,25 @@
 import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri';
 import Button from '../UI/Button';
+import styles from './TodoAction.module.css';
 
-function TodosAction({ resetTodos, deleteCompletedTodos }) {
+function TodosAction({
+  resetTodos,
+  deleteCompletedTodos,
+  completedTodosExist,
+}) {
   return (
-    <>
+    <div className={styles.todosActionsConteiner}>
       <Button title="Reset Todos" onClick={resetTodos}>
         <RiRefreshLine />
       </Button>
-      <Button title="Clear Completed Todos" onClick={deleteCompletedTodos}>
+      <Button
+        title="Clear Completed Todos"
+        onClick={deleteCompletedTodos}
+        disabled={!completedTodosExist}
+      >
         <RiDeleteBin2Line />
       </Button>
-    </>
+    </div>
   );
 }
 
